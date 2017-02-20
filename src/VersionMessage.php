739fb34ca05c3ce42a0196e6484cba1d5d920085
Anton Timmermans <email@atimmer.com>
2017-02-20 13:32:09 +0100

    Add some tests for the versionMessageControl

--- src/VersionMessage.php
+++ src/VersionMessage.php
@@ -17,6 +17,43 @@ class VersionMessageControl {
 		$this->versionDetector = $versionDetector;
 		$this->messagePresenters = $messagePresenters;
 	}
+	/**
+	 * Requires a certain version for the given version detector and show a message using the message presenter.
+	 *
+	 * @param string $version
+	 */
+	public function requireVersion( $version ) {
+		$current_version = $this->versionDetector->detect();
+		$versionRelation = version_compare( $current_version, $version );
+
+		if ( ! $this->isStatisfied( $version ) ) {
+			$this->showMessages();
+		}
+	}
+
+	/**
+	 * Returns if the given version is statisfied by the installed version
+	 *
+	 * @param string $required_version The required version.
+	 * @returns bool
+	 */
+	public function isStatisfied( $required_version ) {
+		$current_version = $this->versionDetector->detect();
+
+		return version_compare( $current_version, $required_version, ">=" );
+	}
+
+	/**
+	 * Shows the version message to the user with all messengers.
+	 *
+	 * @param string $message The message to show to the user.
+	 */
+	public function showMessage( $message ) {
+		foreach ( $this->messagePresenters as $messagePresenter ) {
+			$messagePresenter->show( $message );
+		}
+	}
+
 
 	/**
 	 * Returns the configured version detector
@@ -28,12 +65,26 @@ class VersionMessageControl {
 	}
 
 	/**
+	 * @param VersionDetector $versionDetector
+	 */
+	public function setVersionDetector( $versionDetector ) {
+		$this->versionDetector = $versionDetector;
+	}
+
+	/**
 	 * Returns the configured message presenters
 	 *
 	 * @return MessagePresenter
 	 */
 	public function getMessagePresenters() {
 		return $this->messagePresenters;
+	}
+
+	/**
+	 * @param MessagePresenter[] $messagePresenters
+	 */
+	public function setMessagePresenters( $messagePresenters ) {
+		$this->messagePresenters = $messagePresenters;
 	}
 }
 
--- tests/VersionMessageTest.php
+++ tests/VersionMessageTest.php
@@ -1,17 +1,98 @@
 <?php
 
+class StaticVersionDetector implements VersionDetector {
+	public function __construct( $version ) {
+		$this->version = $version;
+	}
 
+	public function detect() {
+		return $this->version;
+	}
+}
+
+class MessageCollector implements MessagePresenter {
+	protected $messages = array();
+
+	public function show( $message ) {
+		$this->messages[] = $message;
+	}
+
+	public function getMessages() {
+		return $this->messages;
+	}
+}
+
+/**
+ * @property VersionMessageControl versionMessageControl
+ * @property StaticVersionDetector versionDetector
+ */
 class VersionMessageControlTest extends PHPUnit_Framework_TestCase {
 
+	public function setUp() {
+		$this->versionDetector = new StaticVersionDetector( '1.0' );
+
+		$this->versionMessageControl = new VersionMessageControl(
+			$this->versionDetector,
+			array()
+		);
+	}
+
 	public function testConstructor() {
-		$versionDetector = new PHPVersionDetector();
 
 		$versionMessageControl = new VersionMessageControl(
-			new PHPVersionDetector(),
+			$this->versionDetector,
 			array()
 		);
 
-		$this->assertEquals( $versionDetector, $versionMessageControl->getVersionDetector() );
+		$this->assertEquals( $this->versionDetector, $versionMessageControl->getVersionDetector() );
 		$this->assertEquals( array(), $versionMessageControl->getMessagePresenters() );
+	}
+
+	/**
+	 *
+	 */
+	public function testRequireVersion() {
+		$versionMessageControl = new VersionMessageControl(
+			$this->versionDetector,
+			array()
+		);
+
+		$versionMessageControl->requireVersion( "1.0" );
+	}
+
+	/**
+	 * @dataProvider dataIsStatisfied
+	 */
+	public function testIsStatisfied( $expected, $installedVersion, $requiredVersion ) {
+		$this->versionMessageControl->setVersionDetector( new StaticVersionDetector( $installedVersion ) );
+
+		$this->assertEquals( $expected, $this->versionMessageControl->isStatisfied( $requiredVersion ) );
+	}
+
+	public function testShowMessages() {
+		$messenger1 = new MessageCollector();
+		$messenger2 = new MessageCollector();
+
+		$this->versionMessageControl->setMessagePresenters( array( $messenger1 ) );
+
+		$this->versionMessageControl->showMessage( "Message1" );
+		$this->versionMessageControl->setMessagePresenters( array( $messenger1, $messenger2 ) );
+
+		$this->versionMessageControl->showMessage( "Message2" );
+
+		$this->assertEquals( array( "Message1", "Message2" ), $messenger1->getMessages() );
+		$this->assertEquals( array( "Message2" ), $messenger2->getMessages() );
+	}
+
+	public function dataIsStatisfied() {
+		return array(
+			array( true, '1.0', '1.0' ),
+			array( false, '1.0', '1.1' ),
+			array( true, '1.0', '0.9' ),
+			array( false, '1.0-alpha1.0', '1.0' ),
+			array( false, '1.0-beta2.0', '1.0' ),
+			array( true, '1.0-alpha1.0', '0.99' ),
+			array( true, '1.0-beta2.0', '0.99' ),
+		);
 	}
 }
