8554d0e2f17a97cd3425f63328f54dd6737d35ba
Andy Meerwaldt <andy@yoast.com>
2017-07-24 13:49:57 +0200

    Remove unnecessary file.

--- tests/MessageDismisserTest.php
+++ tests/MessageDismisserTest.php
@@ -58,37 +58,4 @@ class MessageDismisserTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertFalse( $dismisser->isDismissed( '>=5.4', 'php' ) );
 	}
-
-	/**
-	 * @dataProvider filterProvider
-	 *
-	 * @param       $expirationDate
-	 * @param array $requirementsToFilter
-	 * @param array $requirements
-	 * @param null  $expected
-	 */
-	public function testFilter( $expirationDate, array $requirementsToFilter, array $requirements, $expected = null ) {
-		if( $expected === null ) {
-			$expected = $requirements;
-		}
-		$filter = new Whip_RequirementsFilter( $expirationDate, $requirementsToFilter );
-		$requirements = $filter->filter( $requirements );
-
-		$this->assertEquals( $expected, $requirements );
-	}
-
-	public function filterProvider() {
-
-		$currentTime = time();
-
-		return array(
-			array( $currentTime, array( 'php' => array( '>=5.4' => $currentTime - 3600 * 24 * 101 ) ), array( 'php' => '>=5.4' ), array() ),
-			array( $currentTime, array( 'php' => array( '>=5.4' => $currentTime + 100 ) ), array( 'php' => '>=5.4' ) ),
-			array( $currentTime, array(), array( 'php' => '>=5.4' ) ),
-			array( $currentTime, array( 'php' => array() ), array( 'php' => '>=5.4' ) ),
-			array( $currentTime, array( 'php' => array( '>=5.2' => $currentTime ) ), array( 'php' => '>=5.4' ) ),
-		);
-
-	}
-
 }
