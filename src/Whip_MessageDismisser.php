d63b3e532099c06750a7f20176ff5c48aa8ff131
Andy Meerwaldt <andy@yoast.com>
2017-07-24 14:49:53 +0200

    Reduces complexity

--- src/Whip_MessageDismisser.php
+++ src/Whip_MessageDismisser.php
@@ -6,110 +6,36 @@
  */
 final class Whip_MessageDismisser {
 
-	/** @var Whip_DissmisStorage */
+	/** @var Whip_DismissStorage */
 	protected $storage;
 
-	/** @var array */
-	private $dismissed = array();
-
 	/** @var int */
-	private $expirationDate;
+	private $currentVersion;
 
 	/**
 	 * Whip_MessageDismisser constructor.
 	 *
-	 * @param int                 $expirationDate
+	 * @param int                 $currentVersion
 	 * @param Whip_DismissStorage $storage
 	 */
-	public function __construct( $expirationDate, Whip_DissmisStorage $storage ) {
-		$this->expirationDate = $expirationDate;
-		$this->storage        = $storage;
-
-		$this->setDismissed();
+	public function __construct( $currentVersion, Whip_DismissStorage $storage ) {
+		$this->currentVersion   = $currentVersion;
+		$this->storage          = $storage;
 	}
 
 	/**
 	 * Dismisses the message for the given component/version.
-	 *
-	 * @param string $component       The component to dismiss.
-	 * @param string $version_compare The version comparison string.
 	 */
-	public function dismiss( $component, $version_compare ) {
-		if ( ! isset( $this->dismissed[ $component ] ) ) {
-			$this->dismissed[ $component ] = array();
-		}
-
-		$this->dismissed[ $component ][ $version_compare ] = $this->expirationDate;
-
-		$this->storage->save( $this->dismissed );
+	public function dismiss() {
+		$this->storage->set( $this->currentVersion );
 	}
 
 	/**
-	 * Filters the requirement based on the requirements to filter.
-	 *
-	 * @param string $versionComparison The version comparison.
-	 * @param string $component         The component name.
+	 * Checks if the dismissed version is lower then the installation version.
 	 *
 	 * @return bool
 	 */
-	public function isDismissed( $versionComparison, $component ) {
-		if ( ! isset( $this->dismissed[ $component ] ) ) {
-			return false;
-		}
-
-		if ( ! isset( $this->dismissed[ $component ][ $versionComparison ] ) ) {
-			return false;
-		}
-
-		if ( $this->isExpired( $this->dismissed[ $component ][ $versionComparison ] ) ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Sets the dismissed values for current request.
-	 */
-	private function setDismissed() {
-		$dismissedOption = $this->storage->get();
-		$this->dismissed = $this->filterExpired( $dismissedOption );
-		if ( $this->dismissed !== $dismissedOption ) {
-			$this->storage->save( $this->dismissed );
-		}
-	}
-
-	/**
-	 * Filters all expires dismissed values.
-	 *
-	 * @param array $dismissed The dismissed values.
-	 *
-	 * @return array
-	 */
-	private function filterExpired( array $dismissed ) {
-		foreach( $dismissed as $component ) {
-			foreach( $component as $versionCompare => $expirationDate ) {
-				if ( $this->isExpired( $expirationDate ) ) {
-					unset( $dismissed[ $component ][ $versionCompare ] );
-				}
-			}
-
-			if ( empty( $dismissed[ $component ] ) ) {
-				unset( $dismissed[ $component ] );
-			}
-		}
-
-		return $dismissed;
-	}
-
-	/**
-	 * Checks if the dismissed value has been expired.
-	 *
-	 * @param string $expirationDate The expiration date.
-	 *
-	 * @return bool True when expired.
-	 */
-	private function isExpired( $expirationDate ) {
-		return $expirationDate <= time();
+	public function isDismissible() {
+		return version_compare( $this->currentVersion, $this->storage->get(), '>' );
 	}
 }
