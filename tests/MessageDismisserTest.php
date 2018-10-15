459293cf3208dca0c2c29dfa9e7b06a113f12128
Andrea Fercia <a.fercia@gmail.com>
2018-10-15 09:42:04 +0200

    Add period.

--- tests/MessageDismisserTest.php
+++ tests/MessageDismisserTest.php
@@ -11,7 +11,7 @@
 class MessageDismisserTest extends PHPUnit_Framework_TestCase {
 
 	/**
-	 * Instantiates our test class
+	 * Instantiates our test class.
 	 */
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
