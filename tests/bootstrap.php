e012bea35c7b2aae29c9d4be560541848b2261fb
jrfnl <jrfnl@users.noreply.github.com>
2019-05-07 05:56:58 +0200

    Make the unit tests PHPUnit cross-version compatible
    To test the code in this repository on all stable PHP versions, PHPUnit 3.x up to 7.x should be supported.
    
    PHP 5.2 needs PHPUnit 3.x
    PHPUnit 4.x will run on PHP 5.3. - 5.6.
    PHPUnit 5.x will run on PHP 5.6 - 7.1.
    PHPUnit 6.x will run on PHP 7.0 - 7.2.
    PHPUnit 7.x will run on PHP 7.1 - 7.3.
    
    For now, PHPUnit 7.x will also work fine for PHP 7.4 (unstable), so supporting PHPUnit 8.x is not yet necessary.
    Ref: https://phpunit.de/supported-versions.html
    
    As WHIP is about motivating people to upgrade to higher PHP versions, dropping support for PHP 5.2/PHPUnit 3.x is not (yet) a good idea.
    
    With that in mind, the simplest change has been made to allow the unit tests to be cross-version compatible.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,22 @@
  * @package Yoast\WHIP
  */
 
+/*
+ * PHPUnit cross version compatibility.
+ *
+ * The classes in PHPUnit have been renamed to namespaced classes.
+ * The namespaced alias for the TestCase class is available as of PHPUnit 5.4 and as of
+ * PHPUnit 6.0, the non-namespaced versions are no longer available.
+ * As PHPUnit 6.0 requires PHP 7 anyway, the PHP 5.3 function class_alias() will be available,
+ * so we can use it to alias the class back to the old class name.
+ */
+if ( class_exists( 'PHPUnit\Framework\TestCase' ) === true
+	&& class_exists( 'PHPUnit_Framework_TestCase' ) === false
+) {
+	// phpcs:ignore PHPCompatibility.FunctionUse.NewFunctions.class_aliasFound
+	class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' );
+}
+
 if ( file_exists( dirname( __FILE__ ) . '/../vendor/autoload_52.php' ) ) {
 	require_once dirname( __FILE__ ) . '/../vendor/autoload_52.php';
 }
