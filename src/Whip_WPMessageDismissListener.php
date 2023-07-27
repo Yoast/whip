dc4184e04a7410281ca9ef3982f05117fd195edf
Vraja Das <vraja.pro@gmail.com>
2023-07-27 11:03:10 +0200

    php cs fix

--- src/Whip_WPMessageDismissListener.php
+++ src/Whip_WPMessageDismissListener.php
@@ -43,7 +43,7 @@ class Whip_WPMessageDismissListener implements Whip_Listener {
 		$action = ( isset( $_GET['action'] ) && is_string( $_GET['action'] ) ) ? sanitize_text_field( wp_unslash( $_GET['action'] ) ) : null;
 		$nonce  = ( isset( $_GET['nonce'] ) && is_string( $_GET['nonce'] ) ) ? sanitize_text_field( wp_unslash( $_GET['nonce'] ) ) : null;
 
-		if ( $action === self::ACTION_NAME && $this->dismisser->verify_nonce( $nonce, self::ACTION_NAME ) ) {
+		if ( $action === self::ACTION_NAME && $this->dismisser->verifyNonce( $nonce, self::ACTION_NAME ) ) {
 			$this->dismisser->dismiss();
 		}
 	}
