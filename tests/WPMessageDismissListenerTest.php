f0379e83fed9e77ef4fb5527625694c4dbe503af
jrfnl <jrfnl@users.noreply.github.com>
2023-09-24 05:18:18 +0200

    Tests: ensure filenames match the class name [1]
    ... and that the test class name reflects the name of the class under test.

--- tests/WPMessageDismissListenerTest.php
+++ tests/WPMessageDismissListenerTest.php
@@ -10,7 +10,7 @@
  *
  * @coversDefaultClass Whip_WPMessageDismissListener
  */
-final class WPMessageDismissListener extends Whip_TestCase {
+final class WPMessageDismissListenerTest extends Whip_TestCase {
 
 	/**
 	 * Tests the listen method.
